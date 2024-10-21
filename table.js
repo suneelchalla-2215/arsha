4327d5da2d639e4f7dae70481d71b93426a24b2f
suneelchalla-2215 <suneelchalla777@gmail.com>
2024-10-21 11:52:21 +0530

    first commit

---
+++ table.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.logo_icons').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
